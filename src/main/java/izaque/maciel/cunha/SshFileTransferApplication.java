91bee25450e433c7847e18f973b0e309708cbc0c
izaquemacielcunha <izaquemaciel@hotmail.com>
2022-09-13 23:44:37 -0300

    Initial commit

---
+++ src/main/java/izaque/maciel/cunha/SshFileTransferApplication.java
@@ -0,0 +1,13 @@
+package izaque.maciel.cunha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SshFileTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SshFileTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/izaque/maciel/cunha/SshFileTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package izaque.maciel.cunha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SshFileTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
